13227135bcfe5146bc120e4f7c24a8eea9f40313
robotkid828 <113220796+robotkid828@users.noreply.github.com>
2022-09-23 15:08:31 -0400

    Add files via upload

---
+++ CloudSet/action.js
@@ -0,0 +1,8 @@
+document.getElementById('cloudset').addEventListener('click', cloudset);
+function send() {
+	var username = document.getElementById('username').value;
+	var pass = document.getElementById('password').value;
+    var projectid = document.getElementById('projectid').value;
+	var cloudname = document.getElementById('cloudname').value;
+	var cloudvalue = document.getElementById('cloudvalue').value;
+}
