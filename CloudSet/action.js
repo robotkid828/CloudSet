9918c7cbca9887a0d646aefcf7f02c4a0587d3ec
robotkid828 <113220796+robotkid828@users.noreply.github.com>
2022-09-23 15:08:19 -0400

    Delete CloudSet directory

--- CloudSet/action.js
+++
@@ -1,8 +0,0 @@
-document.getElementById('cloudset').addEventListener('click', cloudset);
-function send() {
-	var username = document.getElementById('username').value;
-	var pass = document.getElementById('password').value;
-    var projectid = document.getElementById('projectid').value;
-	var cloudname = document.getElementById('cloudname').value;
-	var cloudvalue = document.getElementById('cloudvalue').value;
-}
